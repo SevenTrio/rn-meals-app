fce862ea627007416d0a25e47f62394197af454c
SevenTrio <dimakrikunov12@gmail.com>
2021-10-22 12:34:25 +0300

    Added header buttons to meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
 
 import { MEALS } from '../data/dummy-data';
 
 const MealDetailScreen = ({ route, navigation }) => {
     const { mealId } = route.params;
     const selectedMeal = MEALS.find((m) => m.id === mealId);
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                    <Item
+                        title="Favorite"
+                        iconName="ios-star"
+                        onPress={() => console.log('Mark as favorite!')}
+                    />
+                </HeaderButtons>
+            ),
+        });
+    }, [navigation]);
 
     return (
         <View style={styles.screen}>
