e082e52dc523fb6325c812d2429323c0c93f2460
SevenTrio <dimakrikunov12@gmail.com>
2021-10-23 19:42:38 +0300

    Added the MealDetails screen content

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,9 +1,18 @@
 import React, { useLayoutEffect } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, ScrollView, Image, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import BoldText from '../components/BoldText';
+import DefaultText from '../components/DefaultText';
 import HeaderButton from '../components/HeaderButton';
 
 import { MEALS } from '../data/dummy-data';
+
+const ListItem = ({ children }) => (
+    <View style={styles.listItem}>
+        <DefaultText>{children}</DefaultText>
+    </View>
+);
 
 const MealDetailScreen = ({ route, navigation }) => {
     const { mealId } = route.params;
@@ -24,23 +33,47 @@ const MealDetailScreen = ({ route, navigation }) => {
     }, [navigation]);
 
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-            <Button
-                title="Go Back to Categories"
-                onPress={() => {
-                    navigation.popToTop();
-                }}
-            />
-        </View>
+        <ScrollView>
+            <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}m</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <BoldText style={styles.title}>Ingredients</BoldText>
+            {selectedMeal.ingredients.map((ingredient) => (
+                <ListItem key={ingredient}>{ingredient}</ListItem>
+            ))}
+            <BoldText style={styles.title}>Steps</BoldText>
+            {selectedMeal.steps.map((step) => (
+                <ListItem key={step}>{step}</ListItem>
+            ))}
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+    },
+    title: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        fontSize: 22,
+    },
+    listItem: {
+        marginVertical: 5,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10
     }
 });
 
